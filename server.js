a765b129cc3c60a83cef43338d3dc40fe9086e9f
missfionajean <missfionajean@gmail.com>
2024-11-20 17:06:27 -0500

    set up boilerplate code and established working connection to server and database

---
+++ server.js
@@ -0,0 +1,69 @@
+/* ----------------------------------------------------------- */
+/* ------------------------ Packages ------------------------- */
+/* ----------------------------------------------------------- */
+
+// imports dotenv package and loads environment variable(s)
+const dotenv = require("dotenv");
+dotenv.config();
+
+// imports express and nests in variable for easy use
+const express = require("express");
+const app = express();
+
+// imports mongoose to interface with MongoDB
+const mongoose = require("mongoose");
+
+// imports method-override to allow UPDATE and DELETE HTTP reqs
+const methodOverride = require("method-override");
+
+// imports morgan for better HTTP request logging
+const morgan = require("morgan");
+
+// imports native path package to access static files
+const path = require("path");
+
+/* ----------------------------------------------------------- */
+/* ----------------------- Express.use ----------------------- */
+/* ----------------------------------------------------------- */
+
+/* This middleware parses incoming HTTP request bodies, extracts data into a JavaScript object as "req.body" */
+app.use(express.urlencoded({ extended: false }));
+
+// directs express to use more installed middleware
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+
+// defines static path for CSS styling, image dir, etc
+app.use(express.static(path.join(__dirname, "public")));
+
+/* ----------------------------------------------------------- */
+/* ------------------------ Database ------------------------- */
+/* ----------------------------------------------------------- */
+
+// connects to MongoDB using the connection string in .env file
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+	console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+/* imported data models will go here: const var = require(path) */
+
+/* ----------------------------------------------------------- */
+/* -------------------------- Server ------------------------- */
+/* ----------------------------------------------------------- */
+
+// connects to MongoDB using the connection string in .env file
+mongoose.connect(process.env.MONGODB_URI);
+
+// easily modifiable server port
+const PORT = 3002;
+
+// connecting express to server port
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+/* ----------------------------------------------------------- */
+/* --------------------------- HTTP -------------------------- */
+/* ----------------------------------------------------------- */
